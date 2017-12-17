5ee54c8ca8a69867c4e4958782d1c8eddfcddc9b
Nitin Sharma <nsharma78@gmail.com>
2017-12-17 12:16:29 -0800

    SingleNumber implemented

--- src/main/java/com/nitin/algo/SingleNumber.java
+++ src/main/java/com/nitin/algo/SingleNumber.java
@@ -7,7 +7,19 @@ package com.nitin.algo;
 public class SingleNumber {
 	
 	public static void main(String[] args) {
+		int arr[] = {12, 1, 14, 3, 10, 12, 1, 2, 2, 3, 14};
+		System.out.println("Result: " + findSingle(arr));
+	}
+
+	private static int findSingle(int[] arr) {
+		if(arr == null || arr.length == 0)
+			return Integer.MIN_VALUE;
 		
+		int result = 0;
+		for(int i : arr) 
+			result = result ^ i;
+		
+		return result;
 	}
 
 }
